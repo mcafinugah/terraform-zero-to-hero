e442bd04cd3d05859d62f541596109b244565888
mcafinugah <mcafinugah@gmail.com>
2024-10-27 18:51:44 -0400

    main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -5,4 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-067d488a258342e61"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = subnet- "06aaff480736a5680"
+    key_name = "tycoon-key"
 }
