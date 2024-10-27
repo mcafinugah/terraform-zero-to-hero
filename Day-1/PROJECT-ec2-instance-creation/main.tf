b418cc245ccda34b4214daed69da758efa552901
mcafinugah <mcafinugah@gmail.com>
2024-10-27 18:03:11 -0400

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,8 @@
-provider "aws" {
+provider "aws" 
     region = "us-east-1b"
 }
 
-resource "aws_instance"  {
+resource "aws_instance"  
     ami           = "ami-067d488a258342e61"
     instance_type = "t2.micro"
 }
