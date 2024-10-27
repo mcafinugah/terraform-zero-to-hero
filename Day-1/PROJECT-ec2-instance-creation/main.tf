7a6e75ea76a3d362b651d2ca18db168f214427df
mcafinugah <mcafinugah@gmail.com>
2024-10-27 18:15:16 -0400

    main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,8 @@
-provider "aws" 
-    region = "us-east-1b"
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance"  
-    ami           = "ami-067d488a258342e61"
+resource "aws_instance" "example" {
+    ami           = "ami-067d488a258342e61"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
