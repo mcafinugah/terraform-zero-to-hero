86fa3b636653181c79dc5b9d3a46556c9d5dafcb
mcafinugah <mcafinugah@gmail.com>
2024-10-27 20:00:01 -0400

    main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1"  # Replace with your desired region
 }
 
+# Create an EC2 instance
 resource "aws_instance" "example" {
-    ami           = "ami-i-0c6d19d94c09bbd42"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
-    subnet_id = "subnet-0d474455d4874d47f"
-    key_name = "tycoon-key"
+  ami           = "ami-0c6d19d94c09bbd42" # Example Amazon Linux 2 AMI ID
+  instance_type = "t2.micro"
 }
-
